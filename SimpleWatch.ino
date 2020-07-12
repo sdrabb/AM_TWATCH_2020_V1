90e6df336f1d8612d9396c076c9c8148c666cf8a
Alberto Merciai <albe.merciai@gmail.com>
2020-07-12 16:40:19 +0200

    deleted comments

--- SimpleWatch.ino
+++ SimpleWatch.ino
@@ -1,12 +1,3 @@
-/*
-Copyright (c) 2019 lewis he
-This is just a demonstration. Most of the functions are not implemented.
-The main implementation is low-power standby.
-The off-screen standby (not deep sleep) current is about 4mA.
-Select standard motherboard and standard backplane for testing.
-Created by Lewis he on October 10, 2019.
-*/
-
 // Please select the model you want to use in config.h
 #include "config.h"
 
